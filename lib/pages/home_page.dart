d477927e70903a2d404a52456aefb0d62e6e25e2
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-12 19:43:18 +0600

    working with json file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'dart:convert';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData() async{
+    final catalogJson = await rootBundle.loadString("assets/files/catalog");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
   @override
   Widget build(BuildContext context) {
     final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
