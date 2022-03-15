ce05a6746ad38cd5ffad41ced05d83627483f8fc
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-16 03:20:59 +0600

    Beautiful UI and Vx

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -21,4 +21,8 @@ class MyTheme{
   static ThemeData darkTheme(BuildContext context) => ThemeData(
     brightness: Brightness.dark,
   );
+
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
+
 }
