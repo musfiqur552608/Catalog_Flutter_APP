9805863f6effbc7537b347befbacdafd69b9cd5a
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-21 16:37:26 +0600

    details page and animation added

--- lib/widgets/home_widgets/catalog_list.dart
+++ lib/widgets/home_widgets/catalog_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_detail_page.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter_catalog/models/catalog.dart';
@@ -16,7 +17,17 @@ class CatalogList extends StatelessWidget {
       itemCount: CatalogModel.items.length,
       itemBuilder: (context, index){
         final catalog = CatalogModel.items[index];
-        return CatalogItem(catalog: catalog);
+        return InkWell(
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => HomeDetailPage(
+                        catalog: catalog
+                    ),
+                ),
+            ),
+            child: CatalogItem(catalog: catalog)
+        );
       },
     );
   }
@@ -31,7 +42,10 @@ class CatalogItem extends StatelessWidget {
     return VxBox(
         child: Row(
           children: [
-            CatalogImage(image: catalog.image),
+            Hero(
+                tag: Key(catalog.id.toString()),
+                child: CatalogImage(image: catalog.image)
+            ),
             Expanded(child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
