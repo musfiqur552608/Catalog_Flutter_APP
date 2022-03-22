be3cf7abd340a501a146116ea643282b0a9cc2a2
Musfiq <mushfiqur15-10872@diu.edu.bd>
2022-03-23 03:57:45 +0600

    some code pattarn and icons changed

---
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/cart.dart';
+import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class AddToCart extends StatefulWidget {
+  final Item catalog;
+  const AddToCart({Key? key, required this.catalog}) : super(key: key);
+
+  @override
+  State<AddToCart> createState() => _AddToCartState();
+}
+
+class _AddToCartState extends State<AddToCart> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    final _cart = CartModel();
+    bool isInCart = _cart.items.contains(widget.catalog) ?? false;
+    return ElevatedButton(
+        onPressed: (){
+          if(!isInCart){
+            isInCart = isInCart.toggle();
+            final _catalog = CatalogModel();
+            _cart.catalog = _catalog;
+            _cart.add(widget.catalog);
+            setState(() {});
+          }
+
+        },
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(context.theme.buttonColor),
+            shape: MaterialStateProperty.all(StadiumBorder(),)
+        ),
+        child: isInCart? Icon(Icons.done): Icon(CupertinoIcons.cart_badge_plus),
+    );
+  }
+}
